baaf3d3d47acc308ecfd3dc076d2bb12a9ea5bb4
nghiand <nghiand@vng.com.vn>
2024-01-25 21:51:35 +0700

    add unit tests using mock

--- server/mid/mid_saving/mid_logic_test.go
+++ server/mid/mid_saving/mid_logic_test.go
@@ -103,22 +103,99 @@ func TestOpenSavingAccount(t *testing.T) {
 	defer controller.Finish()
 
 	userServiceMock := pb.NewMockUserServiceClient(controller)
-	mid := CreateMidServiceHandler(userServiceMock, nil)
+	savingServiceMock := pb.NewMockSavingsServiceClient(controller)
+	mid := CreateMidServiceHandler(userServiceMock, savingServiceMock)
 
-	t.Run("Case success", func(t *testing.T) {
+	t.Run("Case error1", func(t *testing.T) {
+		userServiceMock.EXPECT().GetCurrentKYC(gomock.Any(), gomock.Any()).Return(&pb.GetCurrentKYCResponse{
+			UserId:   "",
+			KycLevel: 1,
+		}, status.Error(codes.NotFound, ""))
+
+		expectedErr := status.Error(codes.NotFound, "KYC level verify failed")
+
+		_, err := mid.OpenSavingsAccount(context.TODO(), &pb.OpenSavingsAccountRequest{
+			UserId:      "abv-der",
+			Balance:     1000000,
+			TermType:    "MONTHS",
+			Term:        3,
+			CreatedDate: "01022024",
+		})
+
+		if err.Error() != expectedErr.Error() {
+			t.Errorf("Expected %v but output is %v", expectedErr, err)
+		}
+	})
+
+	t.Run("Case KYC 1", func(t *testing.T) {
 		userServiceMock.EXPECT().GetCurrentKYC(gomock.Any(), gomock.Any()).Return(&pb.GetCurrentKYCResponse{
 			UserId:   "",
 			KycLevel: 1,
 		}, nil)
 
-		expected := 1
+		expectedErr := status.Error(codes.PermissionDenied, "KYC level < 2")
 
-		result, _ := mid.GetCurrentKYC(context.TODO(), &pb.GetCurrentKYCRequest{
-			UserId: "",
+		_, err := mid.OpenSavingsAccount(context.TODO(), &pb.OpenSavingsAccountRequest{
+			UserId:      "abv-der",
+			Balance:     1000000,
+			TermType:    "MONTHS",
+			Term:        3,
+			CreatedDate: "01022024",
 		})
 
-		if result.KycLevel != int32(expected) {
-			t.Errorf("Expected %v but output is %v", expected, result)
+		if err.Error() != expectedErr.Error() {
+			t.Errorf("Expected %v but output is %v", expectedErr, err)
+		}
+	})
+
+	t.Run("Case Open OK 1", func(t *testing.T) {
+		userServiceMock.EXPECT().GetCurrentKYC(gomock.Any(), gomock.Any()).Return(&pb.GetCurrentKYCResponse{
+			UserId:   "",
+			KycLevel: 2,
+		}, nil)
+
+		savingServiceMock.EXPECT().OpenSavingsAccount(gomock.Any(), gomock.Any()).Return(&pb.SavingAccount{
+			UserID:      "abc-123",
+			Balance:     10000000,
+			TermType:    "DAYS",
+			Term:        21,
+			CreatedDate: "01022024",
+			DueDate:     "22022024",
+			Rate:        17500,
+		}, nil)
+
+		res, _ := mid.OpenSavingsAccount(context.TODO(), &pb.OpenSavingsAccountRequest{
+			UserId:      "abc-123",
+			Balance:     10000000,
+			TermType:    "DAYS",
+			Term:        21,
+			CreatedDate: "01022024",
+		})
+
+		expectedRes := &pb.SavingAccount{
+			UserID:      "abc-123",
+			Balance:     10000000,
+			TermType:    "DAYS",
+			Term:        21,
+			CreatedDate: "01022024",
+			DueDate:     "22022024",
+			Rate:        17500,
+		}
+
+		if res.UserId != expectedRes.UserID {
+			t.Errorf("Expected UserID %v but output is %v", expectedRes, res)
+		}
+		if res.Balance != expectedRes.Balance {
+			t.Errorf("Expected balance %v but output is %v", expectedRes, res)
+		}
+		if res.Rate != expectedRes.Rate {
+			t.Errorf("Expected rate %v but output is %v", expectedRes, res)
+		}
+		if res.CreatedDate != expectedRes.CreatedDate {
+			t.Errorf("Expected createdDate %v but output is %v", expectedRes, res)
+		}
+		if res.DueDate != expectedRes.DueDate {
+			t.Errorf("Expected dueDate %v but output is %v", expectedRes, res)
 		}
 	})
 
