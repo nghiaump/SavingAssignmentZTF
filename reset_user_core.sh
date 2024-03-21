ce520dc39cc818136580a3bcbbccc5c2c79fe564
nghiand <nghiand@vng.com.vn>
2024-03-22 00:40:10 +0700

    ignore some directories

--- reset_user_core.sh
+++
@@ -1,8 +0,0 @@
-image_id="docker-core-user:1.0"
-container_id=$(docker container ls -a >/dev/null | awk -v image_id="$image_id" '$2 == image_id {print $1}')
-
-echo "CONTAINER_ID: $container_id"
-echo "IMAGE: $image_id"
-
-docker container remove $container_id
-docker rmi $image_id
